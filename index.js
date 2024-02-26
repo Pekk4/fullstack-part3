65dae2da07be3be055e5c7eaa19d31282dafead4
Pekka <75335748+Pekk4@users.noreply.github.com>
2024-02-26 21:16:19 +0200

    Assignment 3.15

--- index.js
+++ index.js
@@ -38,11 +38,11 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Record.findByIdAndDelete(request.params.id)
+    .then(result => {
+      console.log(result)
+      response.status(204).end()
+    })
 })
 
 app.post('/api/persons', (request, response) => {
